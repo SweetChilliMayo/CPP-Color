13089e7d7efa473e6ee542b5ef41c2411f95d750
Daniel Mayo <s210119@students.aie.edu.au>
2021-04-27 08:59:47 +1000

    README update

---
+++ Colors/Color.cpp
@@ -0,0 +1,22 @@
+#include "Color.h"
+
+namespace Color
+{
+    const char *CLEAR = "\033[0m";
+    const char *BLACK = "\x1B[30m";
+    const char *RED = "\x1B[31m";
+    const char *GREEN = "\x1B[32m";
+    const char *YELLOW = "\x1B[33m";
+    const char *BLUE = "\x1B[34m";
+    const char *MAGENTA = "\x1B[35m";
+    const char *CYAN = "\x1B[36m";
+    const char *WHITE = "\x1B[37m";
+    const char *GREY = "\x1B[90m";
+    const char *BRIGHT_RED = "\x1B[91m";
+    const char *BRIGHT_GREEN = "\x1B[92m";
+    const char *BRIGHT_YELLOW = "\x1B[93m";
+    const char *BRIGHT_BLUE = "\x1B[94m";
+    const char *BRIGHT_MAGENTA = "\x1B[95m";
+    const char *BRIGHT_CYAN = "\x1B[96m";
+    const char *BRIGHT_WHITE = "\x1B[97m";
+}
---
+++ Colors/Color.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace Color
+{
+    extern const char *CLEAR;
+    extern const char *BLACK;
+    extern const char *RED;
+    extern const char *GREEN;
+    extern const char *YELLOW;
+    extern const char *BLUE;
+    extern const char *MAGENTA;
+    extern const char *CYAN;
+    extern const char *WHITE;
+    extern const char *GREY;
+    extern const char *BRIGHT_RED;
+    extern const char *BRIGHT_GREEN;
+    extern const char *BRIGHT_YELLOW;
+    extern const char *BRIGHT_BLUE;
+    extern const char *BRIGHT_MAGENTA;
+    extern const char *BRIGHT_CYAN;
+    extern const char *BRIGHT_WHITE;
+};
